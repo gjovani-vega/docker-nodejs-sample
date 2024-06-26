33b7210acb19aa38f67e67329ac5e07727dca924
Gjovani <gjovani.mirdita@vegait.rs>
2024-06-26 15:57:47 +0200

    removes provider thumbprints

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,8 +1,7 @@
 module "iam_github_oidc_provider" {
-  source                 = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-  additional_thumbprints = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
-  client_id_list         = ["sts.amazonaws.com"]
-  tags                   = var.all_tags
+  source         = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  client_id_list = ["sts.amazonaws.com"]
+  tags           = var.all_tags
 }
 
 module "iam_iam-assumable-role-with-oidc" {
